2978b9cf4f189ce6c6be19f468e065b83f5a4d28
sangmoon100 <tkdans100@naver.com>
2026-02-09 22:36:44 +0900

    fix: specify host in app.listen for web service

--- src/utils/webService.js
+++ src/utils/webService.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 function openPort() {
     app.get('/', (req, res) => res.send('Bot is running!'));
-    app.listen(PORT, () => {
+    app.listen(PORT, '0.0.0.0',() => {
         console.log(`Web service is running on port ${PORT}`);
     });
 }
